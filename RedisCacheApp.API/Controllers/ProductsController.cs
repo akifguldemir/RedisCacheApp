2191706ae3e815cfcae9a4338177f74832659239
Akif Güldemir <akifguldemir@gmail.com>
2025-04-07 00:09:44 +0300

    Add ProductsController and configure repository services
    Created ProductsController for product management with methods to retrieve and create products. Registered IProductRepository in Program.cs and set up an in-memory database context. Configured Swagger for API documentation.

---
+++ RedisCacheApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisCacheApp.API.Models;
+using RedisCacheApp.API.Repository;
+
+namespace RedisCacheApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productRepository.GetAsync()); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productRepository.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            var createdProduct = await _productRepository.CreateAsync(product);
+            return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
+        }
+    }
+}
--- RedisCacheApp.API/Program.cs
+++ RedisCacheApp.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RedisCacheApp.API.Models;
+using RedisCacheApp.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +11,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("myDatabase"));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+} 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
